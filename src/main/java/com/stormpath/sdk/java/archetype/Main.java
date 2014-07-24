c2ae91362e62df7c13d9d0073e9d599f1d049b2d
Mario Antollini <mario@stormpath.com>
2014-07-24 16:57:31 -0300

    Fixed indentation

--- src/main/java/com/stormpath/sdk/java/archetype/Main.java
+++ src/main/java/com/stormpath/sdk/java/archetype/Main.java
@@ -15,7 +15,7 @@ import org.slf4j.LoggerFactory;
  * @since 1.0
  */
 public class Main {
-	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
 
@@ -23,16 +23,16 @@ public class Main {
         //Client
         ApiKey apiKey = ApiKeys.builder().setFileLocation(path).build();
         Client client = Clients.builder().setApiKey(apiKey).build();
-        
+
         logger.debug("Stormpath client has successfully been initialized");
 
-		//Create an application
+        //Create an application
         Application application = client.instantiate(Application.class);
         application.setName("My New Stormpath App!"); //must be unique among your other apps
         application = client.createApplication(Applications.newCreateRequestFor(application).createDirectory().build());
-        
+
         logger.debug("The Application '" + application.getName() + "' has successfully been created in Stormpath with href: " + application.getHref());
-        
+
         //Create an account
         Account account = client.instantiate(Account.class);
         account.setGivenName("John");
@@ -40,8 +40,8 @@ public class Main {
         account.setUsername("johndoe"); //optional, defaults to email if unset
         account.setEmail("johndoe@emaildomainname.com");
         account.setPassword("1234$aBC");
-		account = application.createAccount(account);
-		
+        account = application.createAccount(account);
+
         logger.debug("The Account for '" + account.getUsername() + "' has successfully been created with href: " + account.getHref());
 
     }
